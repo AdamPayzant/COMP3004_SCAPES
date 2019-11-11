d3aeff2d16a82c76fbfa1c43c17ca787a04638c7
FrederykJohannes <57331983+FrederykJohannes@users.noreply.github.com>
2019-11-11 08:22:33 -0500

    Add files via upload

--- main.cc
+++ main.cc
@@ -1,11 +1,15 @@
+#include "./ApplicationLogic/MainController.h"
 #include "./Interface/MainWindow.h"
+#include "./Persistence/PersistenceManager.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+    PersistenceManager persistenceManager;
     MainWindow mainWindow;
+    mainWindow.setWindowTitle("SCAPES");
     mainWindow.show();
-
+    MainController mainController(&persistenceManager, &mainWindow);
     return app.exec();
 }
