f62502db955a539fdb855c582bc069db72e81886
FrederykJohannes <57331983+FrederykJohannes@users.noreply.github.com>
2019-11-12 11:23:10 -0500

    Add files via upload

--- SCAPL/STMTs/DeclIntStmt.h
+++ SCAPL/STMTs/DeclIntStmt.h
@@ -7,30 +7,73 @@
 #include "../Identifier.h"
 #include "../Variable.h"
 
+/**
+ * Program: School of Computer Science Assembly Programming Environment System
+ * Author: //TODO: Team Name
+ * Date: 12/11/2019
+ *
+ * Version 1.0:
+ * File: DeclIntStmt.h
+ * Author: Edward Adam Payzant
+ * Date: 12/11/2019
+ *
+ * File Purpose:
+ *     //////////////
+ *
+ * Formatting style based on course notes and course work from COMP2401 and
+ * COMP2404.
+ * Class design based on course notes and course work from Fall 2018 offering
+ * of COMP2404.
+ *
+ **/
 class DeclIntStmt: public Statement {
     public:
-
+        /**
+         *
+         **/
         DeclIntStmt(Program *);
 
-
+        /**
+         *
+         **/
         ~DeclIntStmt();
 
         /**
-       * Constructor
-       * Parameters:
-       * -  sting repersenting the instruction
-       *
-       **/
+         * Constructor
+         * Parameters:
+         * -  sting repersenting the instruction
+         *
+         **/
         void compile(std::string &);
 
         /**
-     * run function produces the result of the code
-     **/
+         * run function produces the result of the code
+         **/
         void run();
+
+        /**
+         *
+         **/
         virtual std::string getName();
+
+        /**
+         *
+         **/
         virtual Operand* getOperand1();
+
+        /**
+         *
+         **/
         virtual Operand* getOperand2();
+
+        /**
+         *
+         **/
         virtual Label* getLabel();
+
+        /**
+         *
+         **/
         virtual void setLabel(Label *l);
 };
 
