8a8eff9571dff38c98996bb68979638d89d8a133
zacchaeus liang <z.liang111@gmail.com>
2019-12-03 15:04:32 -0500

    did run fuction

---
+++ SCAPL/ReadStmt.cc
@@ -0,0 +1,59 @@
+#include "ReadStmt.h"
+#include "../Program.h"
+
+ReadStmt::ReadStmt(Program *p) {
+    o1 = nullptr;
+    o2 = nullptr;
+    label = nullptr;
+    master = p;
+    subtype = "ReadStmt";
+}
+
+ReadStmt::~ReadStmt() {
+    delete(o1);
+}
+
+void ReadStmt::compile(std::string &line) {
+    int i = line.size() - 1;
+    std::vector<Identifier*> *ids = master->getIds();
+
+    if(line[4] == '$') {
+        // Array case
+        std::string arrName;
+        std::string accessName;
+        int j;
+        for(j = 6; j < line.size(); j++) {
+            if(line.at(j) == '+') {
+                arrName = line.substr(5, j - 5);
+                break;
+            }
+        }
+        accessName = line.substr(j + 1, i - j - 1);
+        for(auto iter = ids->begin(); iter != ids->end(); ++iter) {
+            std::string an;
+            (*iter)->getName(an);
+            if(an.compare(arrName) == 0) {
+                ArrayVariable *downcast = (ArrayVariable *) (*iter);
+                ArrAccess *temp = new ArrAccess(downcast, accessName, master);
+                o1 = new Operand(temp);
+                break;
+            }
+        }
+    }
+    else {
+        // Variable case
+        for(auto iter = ids->begin(); iter != ids->end(); ++iter) {
+            std::string temp;
+            (*iter)->getName(temp);
+            if(temp.compare(line.substr(5, i - 4)) == 0) {
+                o1 = new Operand(*iter);
+            }
+        }
+    }
+}
+
+void ReadStmt::run() {
+string s= master->getUserInput();
+int input = std::stoi( s );
+o2->setVal();
+}
