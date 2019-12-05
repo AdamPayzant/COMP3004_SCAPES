40d34c6c10bbab54e5769831a92ae08a231d5639
FrederykJohannes <57331983+FrederykJohannes@users.noreply.github.com>
2019-12-05 03:00:18 -0500

    Update JLessStmt.cc

--- SCAPL/STMTs/JLessStmt.cc
+++ SCAPL/STMTs/JLessStmt.cc
@@ -48,6 +48,14 @@ void JLessStmt::compile(std::string &line) {
     }
 }
 
+/*
+void JLessStmt::compile(std::string &line) {
+    // Syntax: jls <target label>
+    std::string target = line.substr(3, line.size() - 4);
+    o1 = new Operand(master->getID(target));
+}
+*/
+
 void JLessStmt::run() {
     if(master->getCFlag() == -1) {
         master->changeStmt((Label *) o1->getIDPtr());
