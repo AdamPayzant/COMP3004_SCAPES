7a884248b14df952555d4c7dc3761562b543de38
FrederykJohannes <57331983+FrederykJohannes@users.noreply.github.com>
2019-12-05 07:23:53 -0500

    Update PrintStmt.cc

--- SCAPL/STMTs/PrintStmt.cc
+++ SCAPL/STMTs/PrintStmt.cc
@@ -132,52 +132,6 @@ void PrintStmt::compile(std::string &line) {
     }
 }
 
-/*
-void PrintStmt::compile(std::string &line) {
-    int i = line.size() - 1;
-    std::vector<Identifier*> *ids = master->getIds();
-
-    if(line[4] == '\"' || isdigit(line[4])) {
-        std::string str = line.substr(5, i - 4);
-        Literal *temp = new Literal(str);
-        o1 = new Operand(temp);
-    }
-    else if(line[4] == '$') {
-        // Array Case
-        std::string arrName;
-        std::string accessName;
-        int j;
-        for(j = 6; j < line.size(); j++) {
-            if(line.at(j) == '+') {
-                arrName = line.substr(5, j - 5);
-                break;
-            }
-        }
-        accessName = line.substr(j + 1, i - j - 1);
-        for(auto iter = ids->begin(); iter != ids->end(); ++iter) {
-            std::string an;
-            (*iter)->getName(an);
-            if(an.compare(arrName) == 0) {
-                ArrayVariable *downcast = (ArrayVariable *) (*iter);
-                ArrAccess *temp = new ArrAccess(downcast, accessName, master);
-                o1 = new Operand(temp);
-                break;
-            }
-        }
-    }
-    else {
-        // Variable case
-        for(auto iter = ids->begin(); iter != ids->end(); ++iter) {
-            std::string temp;
-            (*iter)->getName(temp);
-            if(temp.compare(line.substr(5, i - 4)) == 0) {
-                o1 = new Operand(*iter);
-            }
-        }
-    }
-}
-*/
-
 void PrintStmt::run() {
     if(o1 != nullptr && o1->getIDPtr()!=nullptr){
         std::string formattedString;
