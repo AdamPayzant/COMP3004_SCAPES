7053a1cc52c30505ab1a0f5e3d50a2a6e2b5f4e7
FrederykJohannes <57331983+FrederykJohannes@users.noreply.github.com>
2019-12-05 12:55:23 -0500

    Update ReadStmt.cc

--- SCAPL/STMTs/ReadStmt.cc
+++ SCAPL/STMTs/ReadStmt.cc
@@ -107,49 +107,16 @@ void ReadStmt::compile(std::string &line) {
     }
 }
 
-/*
-void ReadStmt::compile(std::string &line) {
-    int i = line.size() - 1;
-    std::vector<Identifier*> *ids = master->getIds();
-
-    if(line[4] == '$') {
-        // Array case
-        std::string arrName;
-        std::string accessName;
-        int j;
-        for(j = 6; j < line.size(); j++) {
-            if(line.at(j) == '+') {
-                arrName = line.substr(5, j - 5);
-                break;
-            }
-        }
-        accessName = line.substr(j + 1, i - j - 1);
-        for(auto iter = ids->begin(); iter != ids->end(); ++iter) {
-            std::string an;
-            (*iter)->getName(an);
-            if(an.compare(arrName) == 0) {
-                ArrayVariable *downcast = (ArrayVariable *) (*iter);
-                ArrAccess *temp = new ArrAccess(downcast, accessName, master);
-                o1 = new Operand(temp);
-                break;
-            }
-        }
-    }
-    else {
-        // Variable case
-        for(auto iter = ids->begin(); iter != ids->end(); ++iter) {
-            std::string temp;
-            (*iter)->getName(temp);
-            if(temp.compare(line.substr(5, i - 4)) == 0) {
-                o1 = new Operand(*iter);
-            }
-        }
-    }
-}
-*/
-
 void ReadStmt::run() {
-    std::string s = master->getUserInput();
-    int input = std::stoi(s);
-    o1->setVal(input);
+    try{
+        std::string s = master->getUserInput();
+        int input = std::stoi(s);
+        o1->setVal(input);
+        return;
+    }catch(std::exception& exceptionRef){
+        std::string errorText = "Runtime Error: Invalid value provided.\n";
+        this->master->pushToWindow(errorText);
+        this->master->end();
+        return;
+    }
 }
